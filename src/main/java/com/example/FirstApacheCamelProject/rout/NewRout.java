50f82ec69f276ea118e31a24c18cf6d9fd1f81c8
MahmoudAbdelfattahQ <mahmouda.fattah96@gmail.com>
2025-01-17 21:20:36 +0200

    #1

--- src/main/java/com/example/FirstApacheCamelProject/rout/NewRout.java
+++ src/main/java/com/example/FirstApacheCamelProject/rout/NewRout.java
@@ -1,14 +1,28 @@
 package com.example.FirstApacheCamelProject.rout;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewRout extends RouteBuilder {
+
+    @Autowired
+    private  PrintMsg printMsg;
+
+
     @Override
     public void configure() throws Exception {
         from("timer:firstname?period=5000")
-                .transform().constant("Hello World")
+              //  .transform().constant("Hello World")
+                .bean(printMsg)
                 .to("log:FirstApacheCamelProject");
     }
+}
+@Component
+class  PrintMsg{
+    public String print(){
+        return  "Hello World !" ;
+    }
+
 }
